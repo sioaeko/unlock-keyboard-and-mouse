02032ffff7f7b2766c5f45a91f3894e4027453a8
Sioaeko <101755125+sioaeko@users.noreply.github.com>
2024-07-11 22:58:11 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,19 +1,36 @@
-/*--------------------------------------------------------------
->>> BACKGROUND
-----------------------------------------------------------------
-# Message listener
---------------------------------------------------------------*/
+// background.js
+const hostnameCache = new Map();
 
-/*--------------------------------------------------------------
-# MESSAGE LISTENER
---------------------------------------------------------------*/
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-	if (message === 'tab-connected') {
-		sendResponse(new URL(sender.url).hostname);
-	} else if (message === 'options-page-connected') {
-		sendResponse({
-			isTab: sender.hasOwnProperty('tab')
-		});
-	}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message === 'tab-connected') {
+    const hostname = getHostname(sender.url);
+    hostnameCache.set(sender.tab.id, hostname);
+    sendResponse(hostname);
+    return true; // Indicates that the response is sent asynchronously
+  } else if (message === 'options-page-connected') {
+    sendResponse({
+      isTab: sender.tab !== undefined
+    });
+    return false; // Indicates that the response is sent synchronously
+  }
 });
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  hostnameCache.delete(tabId);
+});
+
+function getHostname(url) {
+  return new URL(url).hostname;
+}
+
+// Periodic cleanup of hostname cache
+setInterval(() => {
+  chrome.tabs.query({}, (tabs) => {
+    const activeTabIds = new Set(tabs.map(tab => tab.id));
+    for (const [tabId] of hostnameCache) {
+      if (!activeTabIds.has(tabId)) {
+        hostnameCache.delete(tabId);
+      }
+    }
+  });
+}, 30 * 60 * 1000); // Run every 30 minutes
